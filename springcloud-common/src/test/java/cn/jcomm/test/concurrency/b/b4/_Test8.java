9199a3bb046327afdc6d1b35d1136a5835c7b812
66wang066 <66wang066@sina.com>
2018-07-18 18:39:08 +0800

    some test

---
+++ springcloud-common/src/test/java/cn/jcomm/test/concurrency/b/b4/_Test8.java
@@ -0,0 +1,12 @@
+package cn.jcomm.test.concurrency.b.b4;
+
+/**
+ * @author: jowang
+ * @date: 2018-07-11 15:44
+ * @description:
+ */
+public class _Test8 {
+    public static void main(String[] args) {
+
+    }
+}
