5ee955cb8b6a9113dfca27302c20f45b4d326790
linruowang <linruowang@dxy.cn>
2021-11-25 20:10:15 +0800

    feat some demo

---
+++ springcloud-common/src/test/java/cn/jcomm/leetcode/汉明距离.java
@@ -0,0 +1,26 @@
+package cn.jcomm.leetcode;
+
+public class 汉明距离 {
+
+    static class Solution {
+        public static int hammingDistance(int x, int y) {
+            int count = 0;
+            int i = x ^ y;
+            System.out.println("i=" + i);
+            int p = 1;
+            while (i != 0) {
+                if ((i & 1) == 1) {
+                    count++;
+                }
+
+                i = i >> 1;
+                System.out.println(i);
+            }
+            return count;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(hammingDistance(1, 4));
+        }
+    }
+}
---
+++ springcloud-common/src/test/java/cn/jcomm/leetcode/翻转链表.java
@@ -0,0 +1,51 @@
+package cn.jcomm.leetcode;
+
+import java.util.List;
+import java.util.Stack;
+
+public class 翻转链表 {
+
+    public static ListNode reverseList(ListNode head) {
+        //申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while(cur!=null) {
+            //记录当前节点的下一个节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1, new ListNode(2, new ListNode(3)));
+        node = reverseList(node);
+
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
