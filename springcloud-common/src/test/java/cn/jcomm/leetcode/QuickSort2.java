09f226c7d8ce7a71f674c9fd4fdd6ea1b53ea14f
linruowang <linruowang@dxy.cn>
2021-12-11 10:48:18 +0800

    feat 排序

---
+++ springcloud-common/src/test/java/cn/jcomm/leetcode/QuickSort2.java
@@ -0,0 +1,80 @@
+package cn.jcomm.leetcode;
+
+import java.util.Arrays;
+
+public class QuickSort2 {
+
+    /**
+     * 数组内元素交换
+     * @param nums 输入数组
+     * @param i 元素1下标
+     * @param j 元素2下标
+     */
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums [i];
+        nums [i] = nums [j];
+        nums [j] = temp;
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param nums 输入数组
+     * @param left 划分左边界
+     * @param right 划分右边界
+     */
+    private void quickSort(int[] nums, int left, int right) {
+        // 递归返回条件，和分区排序结束
+        if (right-left <=0) {
+            return;
+        }
+        // 选择数组右边界值作为分区节点
+        int pivot = nums[right];
+        // 从数组左边界值开始维护分区
+        int partition=left-1;
+        // 遍历当前分区内元素
+        for (int i = left; i <= right-1; i++) {
+            if ((nums [i] < pivot) ) {
+                // 将小于pivot的值交换到partition左边
+                // 将大于等于pivot的值交换到partition右边
+                partition++;
+                swap(nums, partition, i);
+            }
+        }
+        // 将分区节点插入到数组左右分区中间
+        partition++;
+        swap(nums, partition, right);
+        // 分区节点排序完成
+        // 左分区继续排序，右分区继续排序
+        quickSort(nums,left, partition-1);
+        quickSort(nums,partition+1, right);
+    }
+
+    /**
+     * 排序数组入口函数
+     *
+     * @param nums 输入数组
+     * @return 返回完成排序的数组
+     */
+    public int[] sortArray(int[] nums) {
+        if (nums == null || nums.length ==0) {
+            return nums;
+        }
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        QuickSort2 quickSort2=new QuickSort2();
+        int[] array = {6,72,113,11,23};
+        array= quickSort2.sortArray(array);
+        System.out.println("排序后的结果");
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    // 作者：wonderzlf
+    // 链接：https://leetcode-cn.com/problems/sort-an-array/solution/javakuai-su-pai-xu-jian-xi-by-wonderzlf/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
---
+++ springcloud-common/src/test/java/cn/jcomm/leetcode/最长回文子串.java
@@ -0,0 +1,39 @@
+package cn.jcomm.leetcode;
+
+import org.apache.commons.collections.map.LinkedMap;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class 最长回文子串 {
+
+    static class Solution {
+        public static String longestPalindrome(String s) {
+            if (s == null || s.length() < 1 || s.length() > 1000) {
+                return null;
+            }
+            int maxLen = 1;
+            String maxStr = "";
+
+            for (int i = 0; i < s.length(); i++) {
+
+                // char c = s.charAt(i);
+                // int offset = 0;
+                // int left = i - offset;
+                // int right = i + offset
+                // while (s.charAt(i - 1) == s.charAt(right)) {
+                //     offset
+                //     left > 0 && right < s.length() && s.charAt(i - 1) == s.charAt(right)
+                //     maxLen =
+                // }
+            }
+
+            return maxStr;
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+}
